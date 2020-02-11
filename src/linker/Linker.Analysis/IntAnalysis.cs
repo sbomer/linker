5b2d61c505353603fddc86456af531bd5814455a
Sven Boemer <sbomer@gmail.com>
2020-02-11 17:46:53 +0000

    Buggy ReachesInteresting-LinkerSafe implementation

--- src/linker/Linker.Analysis/IntAnalysis.cs
+++ src/linker/Linker.Analysis/IntAnalysis.cs
@@ -7,9 +7,100 @@ namespace Mono.Linker.Analysis {
         readonly IntCallGraph icg;
         public IntAnalysis(IntCallGraph icg) {
             this.icg = icg;
+            reachesInterestingWithoutGoingThroughLinkerSafe = new int[icg.numMethods];
         }
 
+        int[] reachesInterestingWithoutGoingThroughLinkerSafe;
+//         public bool ReachesInterestingWithoutGoingThroughLinkerSafe(int i) {
+//             // memoize the result
+//             if (reachesInterestingWithoutGoingThroughLinkerSafe[i] != 0) {
+//                 if (reachesInteresting[i] == 1) {
+//                     return true;
+//                 }
+//                 if (reachesInteresting[i] == -1) {
+//                     return false;
+//                 }
+//                 // this happens when we are already in the process of computing the result.
+//                 // temporarily answer false. may be incorrect in presence of cycles?
+//                 return false;
+//             }
+// 
+//             // base case. result depends only on this method.
+//             if (icg.isLinkerSafe[i]) {
+//                 return false;
+//             }
+//             if (icg.isInteresting[i]) {
+//                 return true;
+//             }
+//             if (icg.calles[i] == null) {
+//                 return false;
+//             }
+// 
+//             // recursive case, where result depends on callees.
+//             foreach (var j in icg.callees[i]) {
+//                 if (ReachesInterestingWithoutGoingThroughLinkerSafe(j)) {
+//                     return true;
+//                 }
+//             }
+//             return false;
+//         }
+
         int[] reachesInteresting;
+//        // 0 means we haven't computed a result yet
+        // // 1 means it reaches interesting
+        // // -1 means it doesn't
+        // // 2 means we have started computing the result ()
+        // public bool ReachesInteresting(int i) {
+        //     if (reachesInteresting[i] != 0) {
+        //         // memoized result
+        //         if (reachesInteresting[i] == 1) {
+        //             return true;
+        //         }
+        //         if (reachesInteresting[i] == -1) {
+        //             return false;
+        //         }
+        //         // TODO: I think this is a BUG! if evaluating along a cycle, will first indicate
+        //         // it has started processing everything along the cycle.
+        //         // when reaching the self node again, the direct caller will
+        //         // see this return false. direct caller can then memoize the false result.
+        //         // later when we get back to this method... we've got a problem.
+        //         //System.Diagnostics.Debug.Assert(reachesInteresting[i] == 2);
+        //         // conceptually what needs to happen when we encounter a cycle is,
+        //         // the originator should go on to the next one.
+        //         // but this one should wait on that result.
+// 
+        //         // a better approach: start with interesting, and bubble up. can be O(N^2).
+        //         // we've already started computing the result for this one.
+        //         // and have ended up here again (cycle).
+        //         // in that case, just continue where we left off...
+        //         // no, this doesn't really work.
+        //     }
+        //     // indicate we've started processing this one.
+        //     reachesInteresting[i] = 2;
+        //     if (icg.isInteresting[i]) {
+        //         // interesting methods reach interesting by definition (base case)
+        //         reachesInteresting[i] = 1;
+        //         return true;
+        //     }
+        //     if (icg.callees[i] == null) {
+        //         // no callees means it is not interesting
+        //         reachesInteresting[i] = -1;
+        //         return false;
+        //     }
+        //     // recurse into callees
+        //     int calleeInd = 0;
+        //     foreach (var j in icg.callees[i]) {
+        //         reachesInteresting[i] = 2+calleeInd;
+        //         if (ReachesInteresting(j)) {
+        //             reachesInteresting[i] = 1;
+        //             return true;
+        //         }
+        //         calleeInd++;
+        //     }
+        //     reachesInteresting[i] = -1;
+        //     return false;
+        // }
+
         public bool ReachesInteresting(int i) {
             if (reachesInteresting != null) {
                 return reachesInteresting [i] == 1;
